0b879f46937fbdfee0241debdc1b202823000a12
Michael Critz <michael.critz@sony.com>
2019-03-07 14:05:03 -0800

    Token is now decoupled

--- Sources/App/Models/Token.swift
+++ Sources/App/Models/Token.swift
@@ -1,46 +1,11 @@
-//
-//  Token.swift
-//  App
-//
-//  Created by Michael Critz on 3/5/19.
-//
-
-import Vapor
-import Authentication
-import FluentPostgreSQL
+import Foundation
 
 final class Token: Codable {
     var id: UUID?
     var token: String
-    var userID: User.ID
-    init(token: String, userID: User.ID) {
+    var userID: UUID
+    init(token: String, userID: UUID) {
         self.token = token
         self.userID = userID
     }
-}
-
-extension Token {
-    static func generate(for user: User) throws -> Token {
-        let rando = try CryptoRandom().generateData(count: 16)
-        return try Token(token: rando.base64EncodedString(), userID: user.requireID())
-    }
-}
-
-extension Token: PostgreSQLUUIDModel {}
-extension Token: Content {}
-
-extension Token: Migration {
-    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
-        return Database.create(self, on: connection) { builder in
-            try addProperties(to: builder)
-            builder.reference(from: \.userID, to: \User.id)
-        }
-    }
-}
-
-extension Token: Authentication.Token, BearerAuthenticatable {
-    typealias UserType = User
-    typealias UserIDType = User.ID
-    static let tokenKey: TokenKey = \Token.token
-    static let userIDKey: UserIDKey = \Token.userID
 }
